4a3649ca59e8cc7441244e16337a798117b40751
melchrisbaitanu <s22119020@student.unklab.ac.id>
2025-03-13 10:58:42 -0700

    perubahan fungsi kembali

--- BackButtonHandler.js
+++ BackButtonHandler.js
@@ -1,14 +1,21 @@
 import React, { useEffect } from "react";
 import { Alert, BackHandler, Platform } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const BackButtonHandler = () => {
+  const navigation = useNavigation();
+
   useEffect(() => {
     if (Platform.OS === "android") {
       const backAction = () => {
-        Alert.alert("Konfirmasi", "Apakah Anda ingin keluar dari aplikasi?", [
-          { text: "Batal", style: "cancel" },
-          { text: "Keluar", onPress: () => BackHandler.exitApp() },
-        ]);
+        if (navigation.canGoBack()) {
+          navigation.goBack(); // Kembali ke layar sebelumnya
+        } else {
+          Alert.alert("Konfirmasi", "Apakah Anda ingin keluar dari aplikasi?", [
+            { text: "Batal", style: "cancel" },
+            { text: "Keluar", onPress: () => BackHandler.exitApp() },
+          ]);
+        }
         return true;
       };
 
@@ -18,13 +25,17 @@ const BackButtonHandler = () => {
 
     if (Platform.OS === "web") {
       window.onpopstate = () => {
-        Alert.alert("Konfirmasi", "Apakah Anda ingin kembali?", [
-          { text: "Batal", style: "cancel" },
-          { text: "Kembali", onPress: () => window.history.back() },
-        ]);
+        if (navigation.canGoBack()) {
+          navigation.goBack();
+        } else {
+          Alert.alert("Konfirmasi", "Apakah Anda ingin keluar?", [
+            { text: "Batal", style: "cancel" },
+            { text: "Keluar", onPress: () => window.history.back() },
+          ]);
+        }
       };
     }
-  }, []);
+  }, [navigation]);
 
   return null;
 };
