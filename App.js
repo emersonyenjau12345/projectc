957d6e4b3c60bcc1ce54113dbeb4fc3347dee33a
melchrisbaitanu <s22119020@student.unklab.ac.id>
2025-03-13 11:10:36 -0700

    com

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import BackButtonHandler from "./BackButtonHandler"; // Import komponen tombol kembali
+import BackButtonHandler from "./BackButtonHandler";
 
 import HomeScreen from "./components/HomeScreen";
 import StudentLoginScreen from "./components/StudentLoginScreen";
@@ -15,7 +15,7 @@ import VillageDeanDashboardScreen from "./components/VillageDeanDashboardScreen"
 import ForgotPasswordScreen from "./components/ForgotPasswordScreen";
 import WorkLoginScreen from "./components/WorkLoginScreen";
 import ChatScreen from "./components/ChatScreen";
-import VillageChating from './components/VillageChating';
+import VillageChating from "./components/VillageChating";
 import EditinPihakKerja from "./components/EditinPihakKerja";
 
 const Stack = createStackNavigator();
@@ -23,7 +23,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <BackButtonHandler /> {/* Menangani tombol kembali di semua layar */}
+      <BackButtonHandler /> {/* ✅ Menangani tombol kembali di semua layar */}
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="StudentLogin" component={StudentLoginScreen} />
